3979f8f4e8a09511605cc5faf85e5dff20cdf9cf
Quentin Lim <quentinlim384@gmail.com>
2025-01-16 11:46:45 +0100

    fix ,

--- src/database_handling/database_handling.py
+++ src/database_handling/database_handling.py
@@ -158,7 +158,7 @@ class DBHandling:
                 categorie_prix TEXT,
                 latitude REAL,
                 longitude REAL,
-                FOREIGN KEY (arrondissement) REFERENCES arrondissement(arrondissement);
+                FOREIGN KEY (arrondissement) REFERENCES arrondissement(arrondissement),
                 UNIQUE (nom, adresse) 
             )
             """)
